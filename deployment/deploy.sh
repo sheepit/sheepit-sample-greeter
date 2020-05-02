ac18427069ce1f8cf7dab141bb5c2f183a39531a
redss <ryszard.tarajkowski@gmail.com>
2020-05-02 12:30:54 +0200

    Support for many environments.

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+cat namespace.yaml \
+| sed "s/{{GREETER_K8S_NAMESPACE}}/$GREETER_K8S_NAMESPACE/g" \
+| kubectl apply -f -
+
 cat kubernetes.yaml \
 | sed "s/{{GREETER_NAME}}/$GREETER_NAME/g" \
 | sed "s/{{GREETER_PORT}}/$GREETER_PORT/g" \
-| kubectl apply -f -
+| kubectl apply --namespace=$GREETER_K8S_NAMESPACE -f -
